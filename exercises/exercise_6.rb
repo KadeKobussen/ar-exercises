d799810377ca720b1937a9e031ec71d9976cc049
Kade Kobussen <kadekobussen50@gmail.com>
2023-05-01 19:27:27 -0700

    create many employees in exercise six

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,6 +9,3 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
-@over_1M_stores = Store.where("annual_revenue >= 1000000").count
-
-puts "There are #{@over_1M_stores} stores with over one million dollars in profit this year!"
