1bfb753029090505b17ebc9300c3c0fdd7ccde00
Kade Kobussen <kadekobussen50@gmail.com>
2023-05-01 19:20:01 -0700

    move what was in exercise 6 to 5 where it should be

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -10,7 +10,8 @@ puts "----------"
 @store_count = Store.count
 @total_revenue = Store.sum(:annual_revenue)
 @average_store_revenue = @total_revenue / @store_count
+@over_1M_stores = Store.where("annual_revenue >= 1000000").count
 
 puts "Company total anual revenue: #{@total_revenue}"
 puts "Average annual revenue for all stores: #{@average_store_revenue}"
-
+puts "There are #{@over_1M_stores} stores with over one million dollars in profit this year!"
